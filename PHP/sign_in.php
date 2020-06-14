e0b28903840d3cb436305c7ee5aec80da368fd19
Tapiwa <55806321+Tapiwamazibuko@users.noreply.github.com>
2020-06-14 16:03:53 +0200

    Update sign_in.php

--- PHP/sign_in.php
+++ PHP/sign_in.php
@@ -8,45 +8,9 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
   </head>
   <body>
-    <?php
+  
 
-
-      if (isset($_POST['submit'])) {
-        // Check if name has been entered
-        if (empty($_POST['user'])) {
-            $errName = 'Please enter your user name';
-        }
-        // Check if email has been entered and is valid
-        else if (empty($_POST['email'])) {
-            $errEmail = 'Please enter a valid email address';
-        }
-        // check if a password has been entered and if it is a valid password
-        else if (empty($_POST['password']) || (preg_match("/^.*(?=.{8,})(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z]).*$/", $_POST["password"]) === 0)) {
-            $errPass = '<p class="errText">Password must be at least 8 characters and must contain at least one lower case letter, one upper case letter and one digit</p>';
-        }
-        else {
-            echo "The form has been submitted";
-            alert("here");
-            session_start();
-            $data = array("username" => $_POST['email'], "password" => $_POST['password']);
-            $data_string = json_encode($data);
-
-            $ch = curl_init('http://localhost:8080/phpcallback');
-            curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-            curl_setopt($ch, CURLOPT_POSTFIELDS, $data_string);
-            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-            curl_setopt($ch, CURLOPT_HTTPHEADER, array(
-                'Content-Type: application/json',
-                'Content-Length: ' . strlen($data_string))
-            );
-
-            echo curl_exec($ch)."\n";
-            curl_close($ch);
-        }
-    }
-     ?>
-
-    <form role="form" method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>">
+    <form role="form" method="post" action="post-sign.php">
       <div class="form-group row">
         <label for="inputEmail" class="col-sm-2 col-form-label">Email</label>
         <div class="col-sm-10">

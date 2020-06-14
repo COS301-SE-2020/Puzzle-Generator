76be98fa741258face0818e5e84894f56975d377
Jaynill Gopal <u15175295@tuks.co.za>
2020-06-14 16:27:13 +0200

    fix syntax error

--- PHP/config.php
+++ PHP/config.php
@@ -1,4 +1,4 @@
-<? php
+<?php
 $db = pg_connect("host=ec2-176-34-114-78.eu-west-1.compute.amazonaws.com dbname=d8al2oqsu0r3qc user=dmkrncobzlbxty password=
 56a0b4aa37594eb2b4a12136dcb4cc67ec2524b9edcebd9d9009580b8d922a96");
 

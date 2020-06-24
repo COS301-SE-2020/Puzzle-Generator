d47bea47809ab1067dc9e0c9abdfc7575952e89d
Jaynill Gopal <u15175295@tuks.co.za>
2020-06-24 23:27:08 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ const PORT = process.env.port || 3200
 http.createServer(function (req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.end('Hello World!');
-}).listen(PORT); 
+}).listen(process.env.port || 3200); 

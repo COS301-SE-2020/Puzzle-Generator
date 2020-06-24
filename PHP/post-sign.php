c87209008e39af8b5ea41de71013cd71ee635add
Tapiwa <55806321+Tapiwamazibuko@users.noreply.github.com>
2020-06-24 14:18:07 +0200

    Update post-sign.php

--- PHP/post-sign.php
+++ PHP/post-sign.php
@@ -27,7 +27,36 @@
         );
 
         echo curl_exec($ch)."\n";
+        $result = curl_exec($ch);
+        $json = json_decode($result, true);
         curl_close($ch);
+        // Set session variables
+        $_SESSION["token"] = $json["token"];
+        echo "Session variables are set.";
+        $time = $_SERVER['REQUEST_TIME'];
+
+        /**
+        * for a 30 minute timeout, specified in seconds
+        */
+        $timeout_duration = 1800;
+
+        /**
+        * Here we look for the user's LAST_ACTIVITY timestamp. If
+        * it's set and indicates our $timeout_duration has passed,
+        * blow away any previous $_SESSION data and start a new one.
+        */
+        if (isset($_SESSION['LAST_ACTIVITY']) && 
+           ($time - $_SESSION['LAST_ACTIVITY']) > $timeout_duration) {
+            session_unset();
+            session_destroy();
+            session_start();
+        }
+
+        /**
+        * Finally, update LAST_ACTIVITY so that our timeout
+        * is based on it and not the user's login time.
+        */
+        $_SESSION['LAST_ACTIVITY'] = $time;
     }
 }
  ?>

611e66ebc038cc963f5dd26f017e1d92042e446e
Charley817 <54103503+Charley817@users.noreply.github.com>
2020-06-29 14:16:13 +0200

    Add files via upload

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -28,6 +28,8 @@ import {MatListModule} from '@angular/material/list';
 import { IndexComponent } from './pages/index/index.component';
 import { SignupComponent } from './pages/signup/signup.component';
 import { LoginComponent } from './pages/login/login.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { RatingsComponent } from './pages/ratings/ratings.component';
 
 @NgModule({
   declarations: [
@@ -35,6 +37,8 @@ import { LoginComponent } from './pages/login/login.component';
     IndexComponent,
     SignupComponent,
     LoginComponent,
+    NavbarComponent,
+    RatingsComponent,
   ],
   imports: [
     BrowserModule,

26c275255e23beb42c42e702f0632d2293339fbe
Charley817 <54103503+Charley817@users.noreply.github.com>
2020-06-29 23:32:22 +0200

    Update app-routing.module.ts

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,14 @@ import { Routes, RouterModule } from '@angular/router';
 import { SignupComponent } from './pages/signup/signup.component';
 import { IndexComponent } from './pages/index/index.component';
 import { LoginComponent } from './pages/login/login.component';
+import { RatingsComponent } from './pages/ratings/ratings.component';
 
 
 const routes: Routes = [
   {path: 'index', component: IndexComponent},
   {path: 'signup', component: SignupComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'ratings', component: RatingsComponent},
   
     // otherwise redirect to home
   { path: '', redirectTo: '/index', pathMatch: 'full' },

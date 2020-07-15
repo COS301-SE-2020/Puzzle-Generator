516e8676f4b5a7b64ceb65124245e6ea93ddca9b
Jaynill Gopal <u15175295@tuks.co.za>
2020-07-15 23:15:25 +0200

    Fixing links because apparently case sensitivity -_-

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { User } from '../models/user';
+import { User } from '../models/User';
 import { Puzzle } from '../models/Puzzle';
 import { PuzzleRating } from '../models/PuzzleRating';
 import {HttpHeaders} from '@angular/common/http';
@@ -33,7 +33,7 @@ export class APIService {
   }
 
   getUser(token: any){
-    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'} ) }; 
+    const httpOptions = { headers: new HttpHeaders({ 'Content-Type': 'application/json'} ) };
     return this.http.post('http://localhost:3200/api/users/getUser',token, httpOptions);
   }
 

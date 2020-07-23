78ac1f45187f60c9cd373101b4489c8b2d1a5f54
Jaynill Gopal <u15175295@tuks.co.za>
2020-07-23 15:15:20 +0200

    Deleted old files going to re merge from updated branches

--- index.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-const PORT = process.env.port || 3200
-
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end('Hello World!');
-}).listen(process.env.port || 3200); 

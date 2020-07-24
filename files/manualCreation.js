ed176e08528cf204930060505c0f47949e2c9e23
Yuval Langa <55784161+Glasses-kun@users.noreply.github.com>
2020-07-24 12:56:53 +0200

    Cleaned up code and added comments

--- files/manualCreation.js
+++ files/manualCreation.js
@@ -1,36 +1,48 @@
 let apiURL = "http://localhost:3200/api/puzzles/createPuzzle";
-
 let distanceMetric = 'euclidean';
-function setDistanceMetric(metric)
-{
-	distanceMetric = metric;
+
+let puzzleImage;
+let canvas;
+let canvasCoords;
+let width;
+let height;
+let stage;
+let layer;
+let square;
+let sites = [];
+let siteBoundaries = [];
+let precision = 0;
+let colors = ['Plum', 'Tomato', 'Orange', 'Violet', 'Gray', 'MediumSeaGreen', 'LightGray', 'SlateBlue', 'Brown', 'Aquamarine', 'AntiqueWhite', 'Red', 'Green'];
+let generateButtonClicked = false;
+let token = 'KQlH2g5Io_AwCwotB4TUC';
+let piecesJSONObject = {
+	'pieces' : []
+};
+
+///Initialize data once window is done loading
+window.onload = function() {
+	///Only inititalize if container is present
+	if(document.getElementById('container') != null)
+		initializeData();
 }
 
-window.onload = function() {
-	let puzzleImage;
-	let canvas = document.getElementById('container');
-	let canvasCoords = canvas.getBoundingClientRect();
-	let width = canvas.offsetWidth;
-	let height = canvas.offsetHeight;
-	let sites = [];
-	let siteBoundaries = [];
-	let precision = 0;
-	let colors = ['Plum', 'Tomato', 'Orange', 'Violet', 'Gray', 'MediumSeaGreen', 'LightGray', 'SlateBlue', 'Brown', 'Aquamarine', 'AntiqueWhite'];
-	let generateButtonClicked = false;
-	let token = 'KQlH2g5Io_AwCwotB4TUC';
-	let piecesJSONObject = {
-		'pieces' : []
-	};
+///Initialize data and set functions for buttons
+function initializeData() 
+{
+	canvas = document.getElementById('container');
+	canvasCoords = canvas.getBoundingClientRect();
+	width = canvas.offsetWidth;
+	height = canvas.offsetHeight;
 
-	let stage = new Konva.Stage({
+	stage = new Konva.Stage({
 		container: 'container',
 		width: width,
 		height: height,
 	});
 
-	let layer = new Konva.Layer();
+	layer = new Konva.Layer();
 
-	let square = new Konva.Rect({
+	square = new Konva.Rect({
 		x: 0,
 		y: 0,
 		width: width,
@@ -48,6 +60,7 @@ window.onload = function() {
 		{
 			let x = event.clientX - canvasCoords.x;
 			let y = event.clientY - canvasCoords.y;
+			
 			let point = createPoint(x, y);
 			sites.push({
 				x: point.x(),
@@ -61,21 +74,8 @@ window.onload = function() {
 		}
 	});
 
-	function createPoint(x, y)
-	{
-		let point = new Konva.Circle({
-			x: x,
-			y: y,
-			radius: 1,
-			stroke: 'black',
-			strokeWidth: 2,
-		});
-
-		return point;
-	}
-
 	document.getElementById('generatePuzzleButton').addEventListener('mousedown', generatePuzzle);
-	
+
 	document.getElementById('euclideanButton').addEventListener('mousedown', function(){
 		setDistanceMetric('euclidean');
 	});
@@ -85,213 +85,247 @@ window.onload = function() {
 	});
 
 	document.getElementById('tokenLabel').innerHTML = token;
-	
+
 	document.getElementById('saveButton').addEventListener('mousedown', function() {
-		let puzzleName = document.getElementById('puzzleNameInputBox').value;
-		let puzzleDescription = document.getElementById('puzzleDescriptionInputBox').value;
+		savePuzzle(false);
+	});
 
-		let puzzleImage = stage.toDataURL({ pixelRatio: 0.25 });
+	document.getElementById('saveAndSubmitButton').addEventListener('mousedown', function() {
+		// let puzzleImage = stage.toDataURL({ pixelRatio: 0.25 });
+		// document.getElementById('testingImg').src = puzzleImage;
+		savePuzzle(true);
+	});
+}
 
-		let jsonData = {
-			token: token,
-			name: puzzleName,
-			description: puzzleDescription,
-			puzzleObject: piecesJSONObject,
-			image: puzzleImage,
-			shared: false
-		};
+///Create a post ajax request and send it to the API in order to save the user's created puzzle
+function savePuzzle(share)
+{
+	let puzzleName = document.getElementById('puzzleNameInputBox').value;
+	let puzzleDescription = document.getElementById('puzzleDescriptionInputBox').value;
 
-		$.ajax({
-			type: 'POST',
-			url: apiURL,
-			headers: { 
-				'Access-Control-Allow-Origin' : '*' 
-			},
-			contentType: 'application/json',
-			data: JSON.stringify(jsonData),
-			dataType: 'json',
-			success: function(data, status) {
-				console.log(data);
-				console.log(status);
-			},
-			error: function(data, status) {
-				console.log(data);
-				console.log(status);	
-			}
+	let puzzleImage = stage.toDataURL({ pixelRatio: 0.25 });
+
+	let jsonData = {
+		token: token,
+		name: puzzleName,
+		description: puzzleDescription,
+		puzzleObject: piecesJSONObject,
+		image: puzzleImage,
+		shared: share
+	};
+
+	$.ajax({
+		type: 'POST',
+		url: apiURL,
+		headers: { 
+			'Access-Control-Allow-Origin' : '*' 
+		},
+		contentType: 'application/json',
+		data: JSON.stringify(jsonData),
+		dataType: 'json',
+		success: function(data, status) {
+			console.log(data);
+			console.log(status);
+		},
+		error: function(data, status) {
+			console.log(data);
+			console.log(status);	
+		}
+	});
+}
+
+///Set the selected distance metric for the puzzle
+function setDistanceMetric(metric)
+{
+	distanceMetric = metric;
+}
+
+///Create a visual representation of where the user clicks
+function createPoint(x, y)
+{
+	let point = new Konva.Circle({
+		x: x,
+		y: y,
+		radius: 1,
+		stroke: 'black',
+		strokeWidth: 2,
+	});
+
+	return point;
+}
+
+///Calls the necessary functions to generate puzzle's vertiecs as well as the visual representation 
+function generatePuzzle()
+{
+	//Testing data
+	// {"sites":[78,108,300,102,100,247,333,281,184,331],"queries":[]}
+	// sites = [
+	// {
+	// 	x:78,
+	// 	y:108,
+	// },
+	// {
+	// 	x:300,
+	// 	y:102,
+	// },
+	// {
+	// 	x:100,
+	// 	y:247,
+	// },
+	// {
+	// 	x:333,
+	// 	y:281,
+	// },
+	// {
+	// 	x:184,
+	// 	y:331,
+	// }
+	// ];
+
+	///This makes sure that the generate button will only generate once
+	generateButtonClicked = true;
+
+	generateSiteBoundaries();
+	createPieces();
+}
+
+///Creates the puzzle pieces from the siteBoundaries for each site(position selected by the user) 
+function createPieces()
+{
+	let colorCount = colors.length;
+	for(let i = 0; i < sites.length; i++)
+	{
+		let trimmedPoints = trimPoints(siteBoundaries[i]);
+		let piece = new Konva.Line({
+			points: trimmedPoints,
+			stroke: colors[i % colorCount],
+			strokeWidth: 1,
+			draggable: true
 		});
-	});
 
-	document.getElementById('saveAndSubmitButton').addEventListener('mousedown', function(){
-		let puzzleImage = stage.toDataURL({ pixelRatio: 0.25 });
-		document.getElementById('testingImg').src = puzzleImage;
-	});
+		piece.on('mouseover', function (){
+			document.body.style.cursor = 'pointer';
+		});
 
-	function generatePuzzle()
-	{
-		// {"sites":[78,108,300,102,100,247,333,281,184,331],"queries":[]}
-		// sites = [
-		// {
-		// 	x:78,
-		// 	y:108,
-		// },
-		// {
-		// 	x:300,
-		// 	y:102,
-		// },
-		// {
-		// 	x:100,
-		// 	y:247,
-		// },
-		// {
-		// 	x:333,
-		// 	y:281,
-		// },
-		// {
-		// 	x:184,
-		// 	y:331,
-		// }
-		// ];
-		generateButtonClicked = true;
-		generateSiteBoundaries();
-		createPieces();
+		piece.on('mouseout', function() {
+			document.body.style.cursor = 'default';
+		});
+
+		piecesJSONObject.pieces.push(trimmedPoints);
+		layer.add(piece);
 	}
+	layer.draw();
+	piecesJSONObject = JSON.stringify(piecesJSONObject);
+	// console.log(piecesJSONObject);
+}
 
-	function createPieces()
+///Shortens the array of vertices for each puzzle piece
+function trimPoints(pointArray)
+{
+	let trimmedPoints = [];
+	let lastPointCol;
+	let firstPointCol = pointArray[0];
+	let currentRow = pointArray[1];
+
+	trimmedPoints.push(firstPointCol, currentRow);
+
+	for(let index = 0; index < pointArray.length; index+=2)
 	{
-		let colorCount = colors.length;
-		for(let i = 0; i < sites.length; i++)
+		if(currentRow != pointArray[index+1])
 		{
-			let trimmedPoints = trimPoints(siteBoundaries[i]);
-			let piece = new Konva.Line({
-				// points: siteBoundaries[i],
-				points: trimmedPoints,
-				stroke: colors[i % colorCount],
-				strokeWidth: 1,
-				draggable: true
-			});
-
-			piece.on('mouseover', function (){
-				document.body.style.cursor = 'pointer';
-			});
-
-			piece.on('mouseout', function() {
-				document.body.style.cursor = 'default';
-			});
-
-			piecesJSONObject.pieces.push(trimmedPoints);
-			layer.add(piece);
+			trimmedPoints.push(lastPointCol, currentRow);
+			currentRow = pointArray[index+1];
+			firstPointCol = pointArray[index];
+			trimmedPoints.push(firstPointCol, currentRow);
 		}
-		layer.draw();
-		piecesJSONObject = JSON.stringify(piecesJSONObject);
-		// console.log(piecesJSONObject);
-	}
-
-	function trimPoints(pointArray)
-	{
-		let trimmedPoints = [];
-		let lastPointCol;
-		let firstPointCol = pointArray[0];
-		let currentRow = pointArray[1];
-
-		trimmedPoints.push(firstPointCol, currentRow);
-
-		for(let index = 0; index < pointArray.length; index+=2)
+		else
 		{
-			if(currentRow != pointArray[index+1])
-			{
-				trimmedPoints.push(lastPointCol, currentRow);
-				currentRow = pointArray[index+1];
-				firstPointCol = pointArray[index];
-				trimmedPoints.push(firstPointCol, currentRow);
-			}
-			else
-			{
-				lastPointCol = pointArray[index];
-			}
-		}
-
-		return trimmedPoints;
-	}
-
-	function generateSiteBoundaries()
-	{
-		for(let i = 0; i < sites.length; i++)
-			siteBoundaries[i] = [];
-
-		for(let row = 0; row < height; row++)
-		{
-			for(let col = 0; col < width; col++)
-			{
-				let distances = calculateDistancesFromSitesToPoint(col, row, sites);
-				let equidistantPoints = equidistantPointsPresent(distances);
-				// for(let i = 0; i < equidistantPoints.length; i++)
-				// {
-				// 	siteBoundaries[equidistantPoints[i]].push(col);
-				// 	siteBoundaries[equidistantPoints[i]].push(row);
-				// }
-
-				siteBoundaries[equidistantPoints[0]].push(col);
-				siteBoundaries[equidistantPoints[0]].push(row);
-			}
+			lastPointCol = pointArray[index];
 		}
 	}
 
-	function equidistantPointsPresent(distances)
+	return trimmedPoints;
+}
+
+///Generate the boundaries around the sites
+function generateSiteBoundaries()
+{
+	for(let i = 0; i < sites.length; i++)
+		siteBoundaries[i] = [];
+
+	for(let row = 0; row < height; row++)
 	{
-		let returnArray = [];
-		let minimumDistance = Math.min(...distances);
-
-		if(distanceMetric === 'euclidean')
-			minimumDistance = minimumDistance.toFixed(precision) + '';
-
-		let index = 0, counter = 0;
-
-		while(index != -1)
+		for(let col = 0; col < width; col++)
 		{
-			index = distances.indexOf(minimumDistance);
-			if(index != -1)
-			{
-				returnArray.push(index+counter);
-				distances.splice(index, 1);
-				counter++;
-			}
+			let distances = calculateDistancesFromSitesToPoint(col, row, sites);
+			let equidistantPoints = equidistantPointsPresent(distances);
+
+			siteBoundaries[equidistantPoints[0]].push(col);
+			siteBoundaries[equidistantPoints[0]].push(row);
 		}
-
-		return returnArray;
 	}
+}
 
-	function calculateDistancesFromSitesToPoint(xOfPoint, yOfPoint, sitePoints)
+///Determines which site is closest to a pixel position
+function equidistantPointsPresent(distances)
+{
+	let returnArray = [];
+	let minimumDistance = Math.min(...distances);
+
+	if(distanceMetric === 'euclidean')
+		minimumDistance = minimumDistance.toFixed(precision) + '';
+
+	let index = 0, counter = 0;
+
+	while(index != -1)
 	{
-		let result = [];
-		if(distanceMetric === 'euclidean')
+		index = distances.indexOf(minimumDistance);
+		if(index != -1)
 		{
-			for(let i = 0; i < sitePoints.length; i++)
-				result.push(calculateEuclideanDistance(xOfPoint, yOfPoint, sitePoints[i].x, sitePoints[i].y));
+			returnArray.push(index+counter);
+			distances.splice(index, 1);
+			counter++;
 		}
-		else if(distanceMetric === 'manhattan')
-		{
-			for(let i = 0; i < sitePoints.length; i++)
-				result.push(calculateManhattanDistance(xOfPoint, yOfPoint, sitePoints[i].x, sitePoints[i].y));
-		}
-		return result;
 	}
 
-	function calculateEuclideanDistance(point1X, point1Y, point2X, point2Y)
-	{
-		let result = Math.pow( Math.pow( point1X - point2X, 2 ) + Math.pow( point1Y - point2Y, 2 ), 0.5 );
-		return result.toFixed(precision);
-	}
+	return returnArray;
+}
 
+///Calculates the distances from each site to pixel in position (xOfPoint, yOfPoint) and returns an array
+function calculateDistancesFromSitesToPoint(xOfPoint, yOfPoint, sitePoints)
+{
+	let result = [];
+	for(let i = 0; i < sitePoints.length; i++)
+		result.push(calculateDistance(xOfPoint, yOfPoint, sitePoints[i].x, sitePoints[i].y));
 
-	function calculateManhattanDistance(point1X, point1Y, point2X, point2Y)
-	{
-		let resultX = point1X - point2X;
-		if(resultX < 0)
-			resultX *= -1;
-		let resultY = point1Y - point2Y;
-		if(resultY < 0)
-			resultY *= -1;
-		let result = resultX + resultY;
-		return result;
-	}
+	return result;
+}
+
+///Calculates the distance from point1 to point2 using the selected distance metrix
+function calculateDistance(point1X, point1Y, point2X, point2Y)
+{
+	if(distanceMetric === 'euclidean')
+		return calculateEuclideanDistance(point1X, point1Y, point2X, point2Y);
+	else if(distanceMetric === 'manhattan')
+		return calculateManhattanDistance(point1X, point1Y, point2X, point2Y);
+}
+
+///Calculates and returns the Euclidean distance
+function calculateEuclideanDistance(point1X, point1Y, point2X, point2Y)
+{
+	let result = Math.pow( Math.pow( point1X - point2X, 2 ) + Math.pow( point1Y - point2Y, 2 ), 0.5 );
+	return result.toFixed(precision);
+}
+
+///Calculates and returns the Manhattan distance
+function calculateManhattanDistance(point1X, point1Y, point2X, point2Y)
+{
+	let resultX = point1X - point2X;
+	if(resultX < 0)
+		resultX *= -1;
+	let resultY = point1Y - point2Y;
+	if(resultY < 0)
+		resultY *= -1;
+	let result = resultX + resultY;
+	return result;
 }

f2617bb2242c15f1faa6941334a410d14140e0a6
u173Tsholo <u17391718@tuks.co.za>
2020-07-29 14:22:31 +0200

    Addition of info icon

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -38,7 +38,6 @@ export class APIService {
   }
 
   updateName(currentUser: User){
-    console.log("---------");
     const httpOptions = { headers: new HttpHeaders( { 'Content-Type': 'application/json' })};
     return this.http.put('http://localhost:3200/api/users/updateName', currentUser, httpOptions);
   }

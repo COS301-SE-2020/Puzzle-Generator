cdab7179ca8d9e73f3db6db1feb76fb95ae00433
Jaynill Gopal <u15175295@tuks.co.za>
2020-07-30 14:40:49 +0200

    changes

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
-import { User } from '../models/user';
+import { User } from '../models/User';
 import { Puzzle } from '../models/Puzzle';
 import { PuzzleRating } from '../models/PuzzleRating';
 import {HttpHeaders} from '@angular/common/http';

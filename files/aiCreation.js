f3eb9aa8fdb05cbe3d5682358adbe0c2d33748b5
Yuval Langa <55784161+Glasses-kun@users.noreply.github.com>
2020-08-07 01:48:36 +0200

    Initial working version of AI
    - Still undergoing testing and optimization.
    - Still has to be properly integrated to work with manual creation.

--- files/aiCreation.js
+++ files/aiCreation.js
@@ -8,7 +8,6 @@ let Chromosome = {
 let Site = {
 	x: 0,
 	y: 0,
-	siteBoundaries: [],
 	surfaceArea: 0,
 }
 
@@ -19,15 +18,16 @@ let totalSurfaceArea = width * height;
 
 
 let generationSize = 10;
-let maximumIterations = 10;
+let tournamentSize = 4;
+let maximumIterations = 50;
 let seedString = 'apples';
 let numberOfSites = 3;
 let desiredProportions = [0.50, 0.25, 0.25];
 
 
-function newObject(obj)
+function copyObject(obj)
 {
-    return Object.assign({}, obj);
+    return JSON.parse(JSON.stringify(obj));
 }
 
 function xmur3(str) {
@@ -60,55 +60,147 @@ function random(max)
 
 function begin()
 {
-	let currentGeneration, nextGeneration;
+	let currentGeneration = [], nextGeneration = [];
+	let parentX, parentY, children;
 	let x, y;
+	width = width/10;
+	height = height/10;
+	totalSurfaceArea = width * height;
 	// initialPopulation();
+
+	nextGeneration = initializeGeneration();
+	console.log(nextGeneration);
 	for(let generation = 0; generation < maximumIterations; generation++)
 	{
-		currentGeneration = [];
-		for(let i = 0; i < generationSize; i++)
+		currentGeneration = nextGeneration;
+		nextGeneration = [];
+		for(let i = 0; i < generationSize; i+=2)
 		{
-			let newChromosome = {
-				sites: [],
-				fitness: -1,
-			};
-			for(let siteIndex = 0; siteIndex < numberOfSites; siteIndex++)
-			{
-				let tempSite = {
-					x: 0,
-					y: 0,
-					siteBoundaries: [],
-					surfaceArea: 0,
-				};
-				x = random(width + 1);
-				// console.log('x: ' + x);
-				tempSite.x = x;
-				y = random(height + 1);
-				// console.log('y: ' + y);
-				tempSite.y = y;
-				newChromosome.sites.push(tempSite);
-			}
-
-			calculateAndSetFitness(newChromosome);
-			// console.log(newChromosome);
-			currentGeneration.push(newChromosome);
+			parentX = runTournament(currentGeneration);
+			parentY = runTournament(currentGeneration);
+			children = crossover(parentX, parentY);
+			mutate(children[0]);
+			mutate(children[1]);
+			calculateAndSetFitness(children[0]);
+			calculateAndSetFitness(children[1]);
+			nextGeneration.push(...children);
 		}
 
-		// console.log(currentGeneration);
+		console.log(nextGeneration);
 	}
 
 	console.log('done!');
 }
 
+///Randomly selected a site and a movement direction; then proceed to execute movement using a random offset
+function mutate(chromosome)
+{
+	// let siteIndex = random(numberOfSites);
+	let selectedSite = chromosome.sites[random(numberOfSites)];
+	let offset = random(5);
+	let movementDirection = random(4);
 
+	///Move site to the right by offset (increment x by offset), or move to the left if at board edge
+	if(movementDirection == 0)
+	{
+		if(selectedSite.x + offset < width)
+			selectedSite.x = selectedSite.x + offset;
+		else
+			selectedSite.x = selectedSite.x - offset;
+	}
+	///Move site to the left by offset (decrement x by offset), or move to the right if at the board edge
+	else if(movementDirection == 1)
+	{
+		if(selectedSite.x - offset > 0)
+			selectedSite.x = selectedSite.x - offset;
+		else
+			selectedSite.x = selectedSite.x + offset;
+	}
+	///Move site upwards by offset (decrement y by offset), or move downwards if at the board edge
+	else if(movementDirection == 2)
+	{
+		if(selectedSite.y - offset > 0)
+			selectedSite.y = selectedSite.y - offset;
+		else
+			selectedSite.y = selectedSite.y + offset;
+	}
+	///Move site downwards by offset (increment y by offset), or move upwards if at board edge
+	else if(movementDirection == 3)
+	{
+		if(selectedSite.y + offset < height)
+			selectedSite.y = selectedSite.y + offset;
+		else
+			selectedSite.y = selectedSite.y - offset;
+	}
+}
+
+/**
+	Crossover operation; randomly select a site index and swap the sites located on said index. 
+	This function will create two new children, swap their sites and return an array containing them.
+**/
+function crossover(chromosomeX, chromosomeY)
+{
+	let swapIndex = random(numberOfSites);
+	let childX = copyObject(chromosomeX);
+	let childY = copyObject(chromosomeY);
+
+	let tempSite = childX.sites[swapIndex];
+	childX.sites[swapIndex] = childY.sites[swapIndex];
+	childY.sites[swapIndex] = tempSite;
+
+	return [childX, childY];
+}
+
+///Tournament selection of parent for next generation - fittest from a random bunch is returned.
+function runTournament(generation)
+{
+	let chosenChromosome, fittestChromosome = null;
+
+	for(let i = 0; i < tournamentSize; i++)
+	{
+		chosenChromosome = generation[random(generationSize)];
+		if(fittestChromosome == null || chosenChromosome.fitness < fittestChromosome.fitness)
+			fittestChromosome = chosenChromosome;
+	}
+
+	return fittestChromosome;
+}
+
+///Create and return the initial generation of chromosomes
+function initializeGeneration()
+{
+	let currentGeneration = [];
+	for(let i = 0; i < generationSize; i++)
+	{
+		let t1 = performance.now();
+		let newChromosome = copyObject(Chromosome);
+		for(let siteIndex = 0; siteIndex < numberOfSites; siteIndex++)
+		{
+			let tempSite = copyObject(Site);
+			tempSite.x = random(width + 1);
+			tempSite.y = random(height + 1);
+			newChromosome.sites.push(tempSite);
+		}
+		let t2 = performance.now();
+		// console.log('It took ' + (t2-t1).toFixed(3) + 'ms to generate chromosome and sites');
+		
+		t1 = performance.now();
+		calculateAndSetFitness(newChromosome);
+		t2 = performance.now();
+		// console.log('It took ' + (t2-t1).toFixed(3) + 'ms to calculate fitness!');
+		// console.log(newChromosome);
+		currentGeneration.push(newChromosome);
+	}
+
+	return currentGeneration;
+}
 
 ///Calculate and set the fitness of a chromosome
 function calculateAndSetFitness(chromosome)
 {
 	determineAndSetSiteBoundaries(chromosome);
 	// calculateAndSetSurfaceAreas(chromosome);
-	// sortSites(chromosome);
-
+	sortSites(chromosome);
 	let fitness = 0, difference;
 
 	for(let i = 0; i < desiredProportions.length; i++)
@@ -126,14 +218,20 @@ function calculateAndSetFitness(chromosome)
 ///Sort the sites according to surface area from highest to lowest
 function sortSites(chromosome)
 {
-	chromosome.sites.sort(function(siteA, siteB) { 
-		siteB.surfaceArea - siteA.surfaceArea; 
-	});
+	let sites = chromosome.sites;
+	// console.log('Before Sorting: ');
+	// console.log(sites);
+	sites.sort(function(siteA, siteB) { return 	siteB.surfaceArea - siteA.surfaceArea });
+	// console.log('After Sorting: ');
+	// console.log(sites);
 }
 
 ///Generate the boundaries around the sites
 function determineAndSetSiteBoundaries(chromosome)
 {
+	for(let i = 0; i < numberOfSites; i++)
+		chromosome.sites[i].surfaceArea = 0;
+	
 	for(let row = 0; row < height; row++)
 	{
 		for(let col = 0; col < width; col++)

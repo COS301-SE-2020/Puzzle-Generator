32bb669112128c875d990980f0287aa132c5a40b
Jaynill Gopal <u15175295@tuks.co.za>
2020-08-13 10:11:32 +0200

    Changed small typos in routing orders

--- app.js
+++ app.js
@@ -50,12 +50,14 @@ app.set("views",path.join(__dirname,"views"));
  * */
 app.use(bodyParser.json());
 app.use(express.static(__dirname+"/dist/prometheus-puzzles"));
-app.use('/', indexRouter);
+
+//app.use('/', indexRouter);
 app.use('/api/users', usersRouter);
 app.use('/api', apiRouter);
 //app.use('/api/login', loginRouter);
-app.use('api/puzzle', puzzleRouter);
-//app.use('/*',express.static(__dirname+"/dist/prometheus-puzzles"));
+app.use('/api/puzzle', puzzleRouter);
+
+app.use('/*',express.static(__dirname+"/dist/prometheus-puzzles"));
 /**
  * End Angular - Heroku stuff
  */
@@ -63,11 +65,7 @@ app.use('api/puzzle', puzzleRouter);
 app.engine('html', require('ejs').renderFile);
 app.set('view engine', 'html');
 
-app.use('/', indexRouter);
-app.use('/api/users', usersRouter);
-app.use('/api', apiRouter);
-//app.use('/api/login', loginRouter);
-app.use('api/puzzle', puzzleRouter);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

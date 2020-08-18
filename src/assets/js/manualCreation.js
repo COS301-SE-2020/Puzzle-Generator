07100e793bb02732832773c36802730f96a6b6c7
Yuval Langa <55784161+Glasses-kun@users.noreply.github.com>
2020-08-19 00:38:14 +0200

    Fixes and changes to improve create pages' appeal
    -Made the create pages more presentable
    -Fixed a few bugs with create pages
    -Added new color palette as per request: "Shades of Red"

--- src/assets/js/manualCreation.js
+++ src/assets/js/manualCreation.js
@@ -1,6 +1,6 @@
 import Konva from 'konva';
 export { width, height, setSites, setDisableEditMode, initializeData, calculateDistancesFromSitesToPoint, 
-	equidistantPointsPresent, generateSiteBoundaries, createPieces };
+	equidistantPointsPresent, generateSiteBoundaries, createPieces, clearBoard };
 
 let apiURL = "http://localhost:3200/api/puzzles/createPuzzle";
 let distanceMetric = 'euclidean';
@@ -17,7 +17,7 @@ let layer;
 let square;
 let pointsArray = [];
 let sites = [];
-let siteBoundaries = [];
+let siteBoundaries;
 let pieces = [];
 let precision = 0;
 
@@ -32,37 +32,39 @@ let disableEditMode = false;
 let deletePointButtonClicked = false;
 let piecesJSONObject;
 
-/** Tutorial for adding more color palettes: **/
-//Step 1: Make an array with desired colors - can be hex values or names
-let defaultPalette = ['Plum', 'Tomato', 'Orange', 'Violet', 'Gray', 'MediumSeaGreen', 'LightGray', 'SlateBlue', 'Brown', 'Aquamarine', 'AntiqueWhite', 'Red', 'Green'];
-//Step 2: Add the array to the page using this function with the array as a first and the desired name as a second parameter
-addColorPalette(defaultPalette, "Default");
-//Step 3: Profit!!
+let editIcon, deleteIcon;
 
-let shadesOfBluePalette = ['DarkBlue', 'DeepSkyBlue', 'MediumBlue', 'DodgerBlue', 'MidnightBlue', 'RoyalBlue', 'DarkSlateBlue', 'CornflowerBlue', 'SkyBlue', 'PowderBlue'];
-addColorPalette(shadesOfBluePalette, "Shades of Blue");
+///Make an array with desired colors - can be hex values or names
+let defaultPalette = ['Plum', 'Tomato', 'Orange', 'Violet', 'Gray', 'MediumSeaGreen', 'LightGray', 'SlateBlue', 'Brown', 'Aquamarine', 'AntiqueWhite', 'Red', 'Green'];
+///Add the array to the page using this function with the array as a first and the desired name as a second parameter
+addColorPalette(defaultPalette, "Default");
+
+let shadesOfRedPalette = ['Crimson', 'DarkRed', 'Coral', 'FireBrick', 'IndianRed', 'Maroon', 'OrangeRed', 'PaleVioletRed', 'Red', 'Tomato', 'Brown'];
+addColorPalette(shadesOfRedPalette, "Shades Of Red");
 
 let shadesOfGreenPalette = ['Teal', 'MediumSpringGreen', 'LimeGreen', 'ForestGreen', 'MediumSeaGreen', 'LawnGreen', 'PaleGreen', 'GreenYellow', 'Aquamarine'];
 addColorPalette(shadesOfGreenPalette, "Shades of Green");
 
+let shadesOfBluePalette = ['DarkBlue', 'DeepSkyBlue', 'MediumBlue', 'DodgerBlue', 'MidnightBlue', 'RoyalBlue', 'DarkSlateBlue', 'CornflowerBlue', 'SkyBlue', 'PowderBlue'];
+addColorPalette(shadesOfBluePalette, "Shades of Blue");
 ///Add a color palette to the page and needed functionality
 function addColorPalette(arrayOfColors, paletteName)
 {
 	colorPalettes.push(arrayOfColors);
 	// let radioButton = document.createElement('mat-radio-button');
-	let radioButton = document.createElement('input');
-	radioButton.type = 'radio';
-	radioButton.class = 'radio-button';
-	radioButton.value = paletteName;
-	radioButton.innerHTML = paletteName;
-	radioButton.name = 'colorPalette';
-	radioButton.paletteid = paletteCounter++;
-	radioButton.addEventListener('mousedown', function() {
-		selectedPalette = colorPalettes[this.paletteid];
-		changePuzzleColorPalette(selectedPalette);
-	});
+	// let radioButton = document.createElement('input');
+	// radioButton.type = 'radio';
+	// radioButton.class = 'radio-button';
+	// radioButton.value = paletteName;
+	// radioButton.innerHTML = paletteName;
+	// radioButton.name = 'colorPalette';
+	// radioButton.paletteid = paletteCounter++;
+	// radioButton.addEventListener('mousedown', function() {
+	// 	selectedPalette = colorPalettes[this.paletteid];
+	// 	changePuzzleColorPalette(selectedPalette);
+	// });
 
-	radioButtons.push(radioButton);
+	// radioButtons.push(radioButton);
 }
 
 ///Changes the displayed color palette used on the puzzle and re-renders it
@@ -81,6 +83,7 @@ function changePuzzleColorPalette(colors)
 	}
 }
 
+///Randomizes the color palette used on the puzzle
 function randomizePuzzleColorPalette()
 {
 	if(pieces.length > 0)
@@ -111,16 +114,13 @@ function initializeData(appendedString)
 	if(appendedString == undefined)
 		appendedString = '';
 
+	sites = [];
+	siteBoundaries = [];
+
 	disableEditMode = false;
-	
-	piecesJSONObject = {
-		'pieces' : []
-	};
 
 	selectedPalette = defaultPalette;
-	console.log(canvas);
 	canvas = document.getElementById('container'+appendedString);
-	console.log(canvas);
 	stage = new Konva.Stage({
 		container: 'container'+appendedString,
 		width: width,
@@ -157,9 +157,10 @@ function initializeData(appendedString)
 		}
 	});
 
-	addColorPalettePicker(appendedString);
+	// addColorPalettePicker(appendedString);
 
-	document.getElementById('generatePuzzleButton'+appendedString).addEventListener('mousedown', generatePuzzle);
+	if(document.getElementById('generatePuzzleButton') != null)
+		document.getElementById('generatePuzzleButton').addEventListener('mousedown', generatePuzzle);
 
 	document.getElementById('euclideanButton'+appendedString).addEventListener('mousedown', function(){
 		setDistanceMetric('euclidean');
@@ -170,23 +171,45 @@ function initializeData(appendedString)
 	});
 
 	document.getElementById('saveButton'+appendedString).addEventListener('mousedown', function() {
-		savePuzzle(false);
+		savePuzzle(appendedString);
 	});
 
 	if(document.getElementById('deletePointButton') != null)
 	{
+		editIcon = document.getElementById('editPointButtonIcon');
+		deleteIcon = document.getElementById('deletePointButtonIcon');
+		if(editIcon != null)
+			editIcon.remove();
+
 		document.getElementById('deletePointButton').addEventListener('mousedown', function() {
 			deletePointButtonClicked = !deletePointButtonClicked;
 			if(deletePointButtonClicked)
 			{
 				disableEditMode = true;
-				this.innerHTML = 'edit';
+				this.innerHTML = '';
+				this.appendChild(editIcon);
+				this.innerHTML += 'Edit Mode';
 			}
 			else
 			{
 				disableEditMode = false;
-				this.innerHTML = 'delete';
+				this.innerHTML = '';
+				this.appendChild(deleteIcon);
+				this.innerHTML += 'Delete Mode';
 			}
+		});
+	}
+
+	document.getElementById('randomizeColorsButton'+appendedString).addEventListener('mousedown', randomizePuzzleColorPalette);
+
+	let palettes = document.getElementsByClassName('colorPalettes');
+
+	for(let paletteIndex = 0; paletteIndex < palettes.length; paletteIndex++)
+	{
+		palettes[paletteIndex].addEventListener('mousedown', function(){
+			// console.log(this.getAttribute('paletteid'));
+			selectedPalette = colorPalettes[this.getAttribute('paletteid')];
+			changePuzzleColorPalette(selectedPalette);
 		});
 	}
 
@@ -208,17 +231,15 @@ function addColorPalettePicker(appendedString)
 		colorPaletteDiv.appendChild(radioButtons[i]);
 		colorPaletteDiv.appendChild(label);
 	}
-	
-	document.getElementById('randomizeColorsButton'+appendedString).addEventListener('mousedown', randomizePuzzleColorPalette);
 }
 
 ///Create a post ajax request and send it to the API in order to save the user's created puzzle
-function savePuzzle(share)
+function savePuzzle(appendedString)
 {
 	token = document.getElementById('tokenLabel').innerHTML;
 	
-	let puzzleName = document.getElementById('puzzleNameInputBox').value;
-	let puzzleDescription = document.getElementById('puzzleDescriptionInputBox').value;
+	let puzzleName = document.getElementById('puzzleNameInputBox'+appendedString).value;
+	let puzzleDescription = document.getElementById('puzzleDescriptionInputBox'+appendedString).value;
 
 	let puzzleImage = stage.toDataURL({ pixelRatio: 0.25 });
 
@@ -228,7 +249,7 @@ function savePuzzle(share)
 		description: puzzleDescription,
 		puzzleObject: piecesJSONObject,
 		image: puzzleImage,
-		shared: share
+		shared: false
 	};
 
 	$.ajax({
@@ -267,6 +288,17 @@ function setDisableEditMode(bool)
 function setSites(tempSites)
 {
 	sites = tempSites;
+}
+
+///Clear the board
+function clearBoard()
+{
+	piecesJSONObject = {
+		'pieces' : []
+	};
+
+	pieces = [];
+	layer.destroyChildren();
 }
 
 ///Create a visual representation of where the user clicks
@@ -352,7 +384,7 @@ function generatePuzzle()
 		});
 	}
 
-	layer.destroyChildren();
+	clearBoard();
 
 	///This makes sure that the generate button will only generate once
 	disableEditMode = true;
@@ -432,6 +464,7 @@ function trimPoints(pointArray)
 ///Generate the boundaries around the sites
 function generateSiteBoundaries()
 {
+	// console.log(sites);
 	for(let i = 0; i < sites.length; i++)
 		siteBoundaries[i] = [];
 

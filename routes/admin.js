1e358516877749515c8a7254adfb7fd3e64549af
Jaynill Gopal <u15175295@tuks.co.za>
2020-09-12 10:35:11 +0200

    updated files to correlate with new version

--- routes/admin.js
+++ routes/admin.js
@@ -12,99 +12,99 @@ const Puzzle = require('../models/Puzzle');
 
 //get all users
 router.get('/getAllUsers', (request, response) => {
-    User.findAll({
-      order: [
-           ['id', 'DESC'],
-       ],
+  User.findAll({
+    order: [
+      ['id', 'DESC'],
+    ],
+  })
+    .then( users => {
+      response.status(200).send(users);
     })
-        .then( users => {
-            response.status(200).send(users);
-        })
-        .catch( error => {
-            response.status(403).send("Failed due to: ", error);
-        })
+    .catch( error => {
+      response.status(403).send("Failed due to: ", error);
+    })
 });
 
 
 router.delete('/deleteUser/:userID', (request, response) => {
-    const userID = request.params.userID; //req.params.userid
-    User.destroy( { returning: true, raw: true, plain: true, where: { id:  userID } }
-    )
+  const userID = request.params.userID; //req.params.userid
+  User.destroy( { returning: true, raw: true, plain: true, where: { id:  userID } }
+  )
     .then( () => {
-        response.status(200).send("Successfully deleted");
-     })
+      response.status(200).send("Successfully deleted");
+    })
     .catch( error => {
-        response.status(500).send("Server error");
+      response.status(500).send("Server error");
     } );
 });
 
 
 router.get('/getAllPuzzles', (request, response) => {
-    var puzzleJsonObject = [];
-    var puzzlePlaceholder = {};
-    var index = 0;
-    Puzzle.findAll({ raw: true, include: User})
-        .then( puzzles => {
-            var array = puzzles;
-            var totalNumPuzzles = Object.keys(puzzles).length;
-            array.forEach(element => {
-                puzzlePlaceholder = {
-                    "id":element.id,
-                    "name":element.name,
-                    "description": element.description,
-                    "puzzleObject": element.puzzleObject,
-                    "image": element.image,
-                    "createdAt": element.createdAt,
-                    "shared":element.shared,
-                    "creator": element['testUser.name']
-                };
-                ++index;
-                puzzleJsonObject.push(puzzlePlaceholder);
-                if(index == totalNumPuzzles){
-                    response.status(201).send(puzzleJsonObject);
-                }
-            });
-        })
-        .catch( error => {
-            response.status(500).send("Failed to get all puzzles due to: ", error)
-        });
+  var puzzleJsonObject = [];
+  var puzzlePlaceholder = {};
+  var index = 0;
+  Puzzle.findAll({ raw: true, include: User})
+    .then( puzzles => {
+      var array = puzzles;
+      var totalNumPuzzles = Object.keys(puzzles).length;
+      array.forEach(element => {
+        puzzlePlaceholder = {
+          "id":element.id,
+          "name":element.name,
+          "description": element.description,
+          "puzzleObject": element.puzzleObject,
+          "image": element.image,
+          "createdAt": element.createdAt,
+          "shared":element.shared,
+          "creator": element['testUser.name']
+        };
+        ++index;
+        puzzleJsonObject.push(puzzlePlaceholder);
+        if(index == totalNumPuzzles){
+          response.status(201).send(puzzleJsonObject);
+        }
+      });
+    })
+    .catch( error => {
+      response.status(500).send("Failed to get all puzzles due to: ", error)
+    });
 });
 
 
 router.delete('/deletePuzzle/:puzzleID', (request, response) => {
-    const puzzleID = request.params.puzzleID;
-    Puzzle.destroy( { returning: true, raw: true, plain: true, where: { id:  puzzleID } }
-    )
+  const puzzleID = request.params.puzzleID;
+  Puzzle.destroy( { returning: true, raw: true, plain: true, where: { id:  puzzleID } }
+  )
     .then( () => {
-        response.status(200).send("Successfully deleted");
-     })
+      response.status(200).send("Successfully deleted");
+    })
     .catch( error => {
-        response.status(500).send("Server error");
+      response.status(500).send("Server error");
     } );
 });
 
 router.get('/getAllRatings', (request, response) => {
-    PuzzleRating.findAll({
-      include: [ User, Puzzle]
+  PuzzleRating.findAll({
+    include: [ User, Puzzle]
+  })
+    .then( users => {
+      response.status(200).send(users);
     })
-        .then( users => {
-            response.status(200).send(users);
-        })
-        .catch( error => {
-            response.status(403).send("Failed due to: ", error);
-        })
+    .catch( error => {
+      response.status(403).send("Failed due to: ", error);
+    })
 });
 
 
 router.delete('/deleteRatingAdmin/:rateID', (request, response) => {
-    const rateID = request.params.rateID;
-    PuzzleRating.destroy( { returning: true, raw: true, plain: true, where: { id:  rateID } }
-    )
+  const rateID = request.params.rateID;
+  PuzzleRating.destroy( { returning: true, raw: true, plain: true, where: { id:  rateID } }
+  )
     .then( () => {
-        response.status(200).send("Successfully deleted");
-     })
+      response.status(200).send("Successfully deleted");
+    })
     .catch( error => {
-        response.status(500).send("Server error");
+      response.status(500).send("Server error");
     } );
 });
 
